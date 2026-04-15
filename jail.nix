1cfd7a444948748bb016c665b0e5e9c73fbc53d8
deng232 <78670850+deng232@users.noreply.github.com>
2026-04-15 01:50:01 -0400

    at least working

---
+++ jail.nix
@@ -0,0 +1,49 @@
+{
+  pkgs ? import <nixpkgs> { },
+  jail,
+}:
+let
+  codexProfile = pkgs.buildEnv {
+    name = "codex-env-profile";
+    paths = with pkgs; [
+      codex
+      zsh
+      git
+      coreutils
+      findutils
+      gnugrep
+      gnused
+      gawk
+      curl
+    ];
+    ignoreCollisions = true;
+  };
+
+  jail-env = jail "codex-env" "${pkgs.zsh}/bin/zsh" (
+    c: with c; [
+      mount-cwd
+
+      # Nix profile is a symlink tree; the store must be visible too.
+      (readonly "/nix/store")
+      (readonly codexProfile)
+
+      (set-env "PATH" "${codexProfile}/bin")
+      (set-env "HOME" "/tmp")
+      (set-env "SHELL" "${pkgs.zsh}/bin/zsh")
+      network
+      (add-runtime "export BROWSER=firefox")
+      #(set-env "BROWSER" "${firefox}/bin/firefox")
+      open-urls-in-browser # don't work with rootlesskit, firefox can't connect to localhost
+      # If your jail DSL supports argv only:
+      (set-argv [ "-i" ])
+    ]
+  );
+
+  codex-jail-netns = pkgs.writeShellScriptBin "codex-env" ''
+    exec ${pkgs.rootlesskit}/bin/rootlesskit \
+      --net=slirp4netns \
+      ${jail-env}/bin/codex-env
+  '';
+in
+#jail-env
+codex-jail-netns
